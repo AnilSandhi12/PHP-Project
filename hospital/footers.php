70efbb30ff1c86dbb235c62a716e2dcef2044a5f
AnilSandhi12 <anilsandhi01@gmail.com>
2022-10-18 11:18:16 +0530

    add files

---
+++ hospital/footers.php
@@ -0,0 +1,13 @@
+<div class="wrapper col5" style="background-color: gray">
+  <div id="footer">
+
+    <div id="copyright">
+      <p class="fl_left">Copyright &copy; 2021 - All Rights Reserved | </p>
+      <p class="fl_right"></p>
+      <br class="clear" />
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+</body>
+</html>
